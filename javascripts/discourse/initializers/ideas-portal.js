32cb1ddc8907c63316f683f38a27abced4f10a89
Jordan Violet <https://github.com/jtviolet/discourse-prefix-parent-category-names>
2025-04-03 12:51:26 -0400

    chatgpt attempt at fixing

--- javascripts/discourse/initializers/ideas-portal.js
+++ javascripts/discourse/initializers/ideas-portal.js
@@ -210,7 +210,7 @@ export default apiInitializer("0.11.1", (api) => {
             ticks: {
               display: false
             },
-            grid: { display: false },
+            grid: { color: returnPrimaryColor() },
             angleLines: { color: returnPrimaryColor() },
             pointLabels: {
               display: true,
