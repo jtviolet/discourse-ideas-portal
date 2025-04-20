bc9cdebf5dd57a7d3fc59c7e184ce6df397ce153
Jordan Violet <jordan.violet@netwrix.com>
2025-04-19 22:11:29 -0400

    chore: reduce chart polling interval to 1s for faster theme detection

--- javascripts/discourse/initializers/ideas-portal.js
+++ javascripts/discourse/initializers/ideas-portal.js
@@ -590,12 +590,13 @@ export default apiInitializer("0.11.1", (api) => {
   api.cleanupStream(() => bodyObserver.disconnect());
   
   // Fallback: periodically update chart to catch theme changes not detected by observers
+  // Reduced interval to 1 second for faster response
   const chartUpdateInterval = setInterval(() => {
     console.log('IdeasPortal: periodic chart update');
     if (window.ideasStatusChart) {
       window.ideasStatusChart.update();
     }
-  }, 2000);
+  }, 1000);
   api.cleanupStream(() => clearInterval(chartUpdateInterval));
   api.cleanupStream(() => {
     if (window.ideasPortalObserver) {
